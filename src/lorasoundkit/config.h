0f715f846064add4b88c9e9e5b600e7b5b5879eb
Marcel Meek <marcel.meek@gmail.com>
2021-11-03 22:50:30 +0100

    set microphone dependent correction

--- src/lorasoundkit/config.h
+++ src/lorasoundkit/config.h
@@ -12,7 +12,7 @@
 // define in seconds, how often a message will be sent
 #define CYCLETIME   60  // use 150 for RIVM and sensor.community project
 
-// set microphone dependant correction in dB
+// set microphone dependent correction in dB
 // for SPH0645 define -1.8
 // for ICS43434 define 1.5
 // otherwise define 0.0
@@ -23,8 +23,5 @@
 #define APPEUI "70B3D57ED003ED46"
 #define APPKEY "53A4419ACE4EABF7BE9E5FB0B8A16F27"
 // DEVEU is obtained from ESP board id
-
-//api key  (MQTT username)
-//NNSXS.6X7AUQOG4C7URKQ43OLIMZREFTITPGX7ZZ5UNVY.OYACRXFUVBUPIGCS7YN3TY6BOHYEEER5Z6BM3OLZ5PVRIG2TT4DQ
 
 #endif
